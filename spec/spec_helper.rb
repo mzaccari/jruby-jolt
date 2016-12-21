745272cc47c8194d6b4fa9ca69ebdd1ca1671275
Michael Zaccari <michael.zaccari@accelerated.com>
2016-12-21 17:26:47 -0500

    feat: add fixture paths for spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,22 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "jruby-jolt"
+
+def spec_path
+  "#{File.expand_path("../../", __FILE__)}/spec"
+end
+
+def fixtures_path
+  "#{spec_path}/fixtures"
+end
+
+def fixture_path(file)
+  fixtures_path + '/' + file
+end
+
+def fixture(file)
+  File.read(fixture_path(file))
+end
+
+def parse_json(json)
+  MultiJson.load(json)
+end
