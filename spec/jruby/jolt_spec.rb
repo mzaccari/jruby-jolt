e493c347fb4054b15b81d453b836429c6f8923eb
Michael Zaccari <michael.zaccari@accelerated.com>
2016-12-21 17:27:17 -0500

    feat: add basic transformation test

--- spec/jruby/jolt_spec.rb
+++ spec/jruby/jolt_spec.rb
@@ -2,4 +2,29 @@ require 'spec_helper'
 
 describe Jolt do
 
+  describe '.transform' do
+
+    let(:name)   { :test }
+    let(:input)  { parse_json(fixture('input.json')) }
+    let(:spec)   { parse_json(fixture('spec.json')) }
+    let(:output) { parse_json(fixture('output.json')) }
+
+    context 'when given a name that does not exist' do
+      it 'throws an exception' do
+        expect { Jolt.transform(:invalid_name, input) }.to raise_error(Jolt::SpecError)
+      end
+    end
+
+    context 'when the spec exists' do
+      before { Jolt.load_spec(name, spec) }
+      after  { Jolt.clear_specs }
+
+      context 'when given valid input data' do
+        it 'transforms the data' do
+          expect(Jolt.transform(name, input)).to eq output
+        end
+      end
+    end
+  end
+
 end
