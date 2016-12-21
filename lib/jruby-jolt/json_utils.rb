a55f06f09e93b2c5c91cc7deab52c83eacf1128b
Michael Zaccari <michael.zaccari@accelerated.com>
2016-12-21 16:30:05 -0500

    feat: add separate json-utils class and jars

---
+++ lib/jruby-jolt/json_utils.rb
@@ -0,0 +1,10 @@
+require 'java'
+require 'jolt-core'
+require 'jackson-core'
+require 'jackson-databind'
+require 'jackson-annotations'
+require 'jolt-json-utils'
+
+class Jolt::JsonUtils < com.bazaarvoice.jolt.JsonUtils
+
+end
